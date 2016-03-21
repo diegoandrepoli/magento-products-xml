231d7ea89cba04f8b270cf0de184bf824e4f80ee
root <root@localhost.localdomain>
2016-03-20 20:41:18 -0400

    Adicionado plugin Poli XML

---
+++ Poli/XML/Controller/Category/Index.php
@@ -0,0 +1,47 @@
+<?php
+namespace Poli\XML\Controller\Category;
+use Magento\TestFramework\Inspection\Exception;
+
+/**
+ * Index Controller
+ * @package Poli\XML\Controller\Category
+ * @autor Diego Andre Poli <diegoandrepoli@gmail.com>
+ */
+class Index extends \Magento\Framework\App\Action\Action {
+
+    /**
+     * @throws Exception
+     */
+    public function execute() {
+        //parâmetro de url - id
+        $id = $this->getRequest()->getParam('id');
+
+        if(empty($id)){
+            throw new Exception('Categoria não informada');
+        }
+
+        //intância do objeto
+        $object = \Magento\Framework\App\ObjectManager::getInstance();
+        //instância da categoria
+        $category = $object->get('Magento\Catalog\Model\Category');
+        //busca categoria
+        $cat_info = $category ->load($id);
+        //busca produtos da categoria informada
+        $products = $category->getProductCollection()->addCategoryFilter($cat_info)
+            ->addAttributeToFilter('type_id', 'simple')
+            ->addAttributeToSelect('*');
+
+        //elemento XML
+        $xml = new \SimpleXMLElement('<xml/>');
+        //adiciona produtos ao elemento XML
+        foreach($products as $product){
+            $track = $xml->addChild('product');
+            $track->addChild('id', htmlspecialchars($product->getId()));
+            $track->addChild('name', htmlspecialchars($product->getName()));
+        }
+
+        //escreve XML
+        Header('Content-type: text/xml');
+        print $xml->asXML();
+    }
+}
---
+++ Poli/XML/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Poli XML module registration
+ * @autor Diego Andre Poli <diegoandrepoli@gmail.com>
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Poli_XML',
+    __DIR__
+);
